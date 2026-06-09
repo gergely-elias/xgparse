f09ceac4dd44a624e1df057292652c999bd71a70
Gergely Elias <gergely.elias@gmail.com>
2026-06-09 21:27:49 +0200

    narrowing the public surface

--- src/xgparse/__init__.py
+++ src/xgparse/__init__.py
@@ -1,6 +1,2 @@
-from . import convertxg2txt as convertxg2txt
-from . import extractxgdata as extractxgdata
-from . import xgimport as xgimport
-from . import xgstruct as xgstruct
-from . import xgutils as xgutils
-from . import xgzarc as xgzarc
+from xgparse.convertxg2txt import convert_xg_to_text
+from xgparse.extractxgdata import main as extract_xg_data
